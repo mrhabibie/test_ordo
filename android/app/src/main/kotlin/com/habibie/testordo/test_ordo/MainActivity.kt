98a2601c35489e19821632ad2299c4afedc67f3d
Miftahur Rohman Habibie <mrhabibiesmk@gmail.com>
2023-07-01 16:29:35 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/habibie/testordo/test_ordo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.habibie.testordo.test_ordo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/utils/ordo_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OrdoColors {
+  OrdoColors._();
+
+  static const MaterialColor mainPurple = MaterialColor(0xffB07C1A, {});
+  static const MaterialColor darkPurple = MaterialColor(0xff8B3A75, {});
+  static const MaterialColor purple = MaterialColor(0xff7864E6, <int, Color>{
+    100: Color(0xffBEA4F3),
+    50: Color(0xffF1EFFC),
+  });
+  static const MaterialColor lightRed = MaterialColor(0xffEB73A0, {});
+  static const MaterialColor red = MaterialColor(0xffFF5959, {});
+  static const MaterialColor orange = MaterialColor(0xffEDAD60, {});
+  static const MaterialColor green1 = MaterialColor(0xff6BF53A, {});
+  static const MaterialColor green2 = MaterialColor(0xff75DB73, {});
+  static const MaterialColor white = MaterialColor(0xffffffff, {});
+  static const MaterialColor gray1 = MaterialColor(0xfff6f6f6, {});
+  static const MaterialColor gray2 = MaterialColor(0xffAFA7B2, {});
+  static const MaterialColor gray3 = MaterialColor(0xff8D8192, {});
+  static const MaterialColor black = MaterialColor(0xff35203F, {});
+}
